2251cb489cce647b1727976a519992d9eb236e28
Fryden <Fryden@Jrgens-MacBook-Pro.local>
2016-02-08 17:49:33 +0100

    Jobbed on stuff

--- Jørgens Dream/Assets/scripts/userdatabase/database.cs
+++ Jørgens Dream/Assets/scripts/userdatabase/database.cs
@@ -1,118 +1,124 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class database : MonoBehaviour 
+public class database : MonoBehaviour
 {
-    public static string user = "", name = "";
-    private string password = "", rePass = "", message = "";
+	public static string user = "", name = "";
+	private string password = "", rePass = "", message = "";
 
-    private bool register = false;
+	private bool register = false;
 
-    private void OnGUI()
-    {
-        if (message != "")
-            GUILayout.Box(message);
+	private void OnGUI ()
+	{
+		if (message != "")
+			GUILayout.Box (message);
 
-        if (register)
-        {
-            GUILayout.Label("Username");
-            user = GUILayout.TextField(user);
-            GUILayout.Label("Name");
-            name = GUILayout.TextField(name);
-            GUILayout.Label("password");
-            password = GUILayout.PasswordField(password, "*"[0]);
-            GUILayout.Label("Re-password");
-            rePass = GUILayout.PasswordField(rePass, "*"[0]);
+		if (register) {
+			GUILayout.Label ("Username");
+			user = GUILayout.TextField (user);
+			GUILayout.Label ("Name");
+			name = GUILayout.TextField (name);
+			GUILayout.Label ("password");
+			password = GUILayout.PasswordField (password, "*" [0]);
+			GUILayout.Label ("Re-password");
+			rePass = GUILayout.PasswordField (rePass, "*" [0]);
 
-            GUILayout.BeginHorizontal();
+			GUILayout.BeginHorizontal ();
 
-            if (GUILayout.Button("Back"))
-                register = false;
+			if (GUILayout.Button ("Back"))
+				register = false;
 
-            if (GUILayout.Button("Register"))
-            {
-                message = "";
+			if (GUILayout.Button ("Register")) {
+				message = "";
 
-                if (user == "" || name == "" || password == "")
-                    message += "Please enter all the fields \n";
-                else
-                {
-                    if (password == rePass)
-                    {
-                        WWWForm form = new WWWForm();
-                        form.AddField("user", user);
-                        form.AddField("name", name);
-                        form.AddField("password", password);
-                        WWW w = new WWW("http://nordentertainment.com/unitydatabase/register.php", form);
-                        StartCoroutine(registerFunc(w));
-                    }
-                    else
-                        message += "Your Password does not match \n";
-                }
-            }
+				if (user == "" || name == "" || password == "")
+					message += "Please enter all the fields \n";
+				else {
+					if (password == rePass) {
+						WWWForm form = new WWWForm ();
+						form.AddField ("user", user);
+						form.AddField ("name", name);
+						form.AddField ("password", password);
+						WWW w = new WWW ("http://nordentertainment.com/unitydatabase/register.php", form);
+						StartCoroutine (registerFunc (w));
+					} else
+						message += "Your Password does not match \n";
+				}
+			}
 
-            GUILayout.EndHorizontal();
-        }
-        else
-        {
-            GUILayout.Label("User:");
-            user = GUILayout.TextField(user);
-            GUILayout.Label("Password:");
-            password = GUILayout.PasswordField(password, "*"[0]);
+			GUILayout.EndHorizontal ();
+		} else {
+			GUILayout.Label ("User:");
+			user = GUILayout.TextField (user);
+			GUILayout.Label ("Password:");
+			password = GUILayout.PasswordField (password, "*" [0]);
 
-            GUILayout.BeginHorizontal();
+			GUILayout.BeginHorizontal ();
 
-            if (GUILayout.Button("Login"))
-            {
-                message = "";
+			if (GUILayout.Button ("Login")) {
+				message = "";
 
-                if (user == "" || password == "")
-                    message += "Please enter all the fields \n";
-                else
-                {
-                    WWWForm form = new WWWForm();
-                    form.AddField("user", user);
-                    form.AddField("password", password);
-					WWW w = new WWW("http://nordentertainment.com/unitydatabase/login.php", form);
-                    StartCoroutine(login(w));
-                }
-            }
+				if (user == "" || password == "")
+					message += "Please enter all the fields \n";
+				else {
+					WWWForm form = new WWWForm ();
+					form.AddField ("user", user);
+					form.AddField ("password", password);
+					WWW w = new WWW ("http://nordentertainment.com/unitydatabase/login.php", form);
+					StartCoroutine (login (w));
+				}
+			}
 
-            if (GUILayout.Button("Register"))
-                register = true;
+			if (GUILayout.Button ("Register"))
+				register = true;
 
-            GUILayout.EndHorizontal();
-        }
-    }
+			GUILayout.EndHorizontal ();
+		}
+	}
 
-    IEnumerator login(WWW w)
-    {
-        yield return w;
-        if (w.error == null)
-        {
-            if (w.text == "login-SUCCESS")
-            {
-                print("WOOOOOOOOOOOOOOO!");
-            }
-            else
-                message += w.text;
-        }
-        else
-        {
-            message += "ERROR: " + w.error + "\n";
-        }
-    }
+	/*GameObject messageObject;
+	string msgText;
 
-    IEnumerator registerFunc(WWW w)
-    {
-        yield return w;
-        if (w.error == null)
-        {
-            message += w.text;
-        }
-        else
-        {
-            message += "ERROR: " + w.error + "\n";
-        }
-    }
+	void Start ()
+	{
+		GameObject.Find ("MessageText").GetComponent<Text> ().text = "";
+		msgText = "";
+	}
+
+	public void LoginButton (string user, string password)
+	{
+		if (msgText != "") {
+			GameObject.Find ("MessageText").GetComponent<Text> ().text = msgText;
+
+		}
+		if (user == "" || password == "") {
+			
+		}
+	}*/
+
+
+	IEnumerator login (WWW w)
+	{
+		yield return w;
+		if (w.error == null) {
+			if (w.text == "login-SUCCESS") {
+				print ("WOOOOOOOOOOOOOOO! ");
+
+			} else
+				message += w.text;
+		} else {
+			message += "ERROR: " + w.error + "\n";
+		}
+	}
+
+	IEnumerator registerFunc (WWW w)
+	{
+		yield return w;
+		if (w.error == null) {
+			message += w.text;
+		} else {
+			message += "ERROR: " + w.error + "\n";
+		}
+	}
 }
